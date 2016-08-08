85ab31242c8514ee032fd1e89671fb70c41d1a20
ZintisMay <zbmayk@gmail.com>
2016-08-07 23:04:05 -0400

    1st branch

---
+++ app/routes/hb-html-page-objects.js
@@ -0,0 +1,68 @@
+function hbproto(title, paragraph, person, imgsrc, url){
+		this.title = title;
+		this.paragraph = paragraph;
+		this.person = person;
+		this.imgsrc = imgsrc;
+		this.url = url; 
+	}
+
+	var experimental = {
+		title: "Word Wars!",
+		paragraph: "This is a typing game created by our small team of web developers. It features a library of words from the Wordnik API, high scores stored in MySql, fun graphic and sound design. We used Firebase for user authentication, dynamic room creation, a chat service, and multiplayer interaction. We are very proud of our product created in just a few weeks, and intend to continue working on it by adding features in the future.",
+		person: [
+		{image:"static/images/Lou.png", name:"Louis Delia"},
+		{image:"static/images/Matt.png", name:"Matthew Martin"},
+		{image:"static/images/Ana.png", name:"Analben Manakiwala"}
+		],
+		imgsrc:"static/images/wordwars.png",
+		url: "http://wordwarsapp.herokuapp.com"
+	}
+	
+	var experiment = [];
+
+	experiment[1] = new hbproto(
+		"Word Wars!",
+		"This is a typing game created by our small team of web developers. It features a library of words from the Wordnik API, high scores stored in MySql, fun graphics and sound design. We used Firebase for user authentication, dynamic room creation, a chat service, and multiplayer interaction. We are very proud of our product created in just a few weeks, and intend to continue adding features in the future.",
+		[
+			{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAe2AAAAJDdjN2NiMmYyLTFhYjUtNDNlYS1hOWE2LTI0OTgxYTY0ZjVhNw.jpg", name:"Louis Delia", url:"https://www.linkedin.com/in/ltdelia?authType=NAME_SEARCH&authToken=8yFf&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CentityType%3AentityHistoryName%2CclickedEntityId%3Amynetwork_186727910%2Cidx%3A2"},
+			{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAjDAAAAJDA0ZDNiNTdiLTAxODQtNGRjMC1iYTQzLTUxZGFkYzBhYTQxOA.jpg", name:"Matthew Martin", url:"https://www.linkedin.com/in/mnmartin?authType=NAME_SEARCH&authToken=Ptlf&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A53404974%2CauthType%3ANAME_SEARCH%2Cidx%3A1-2-2%2CtarId%3A1470613010323%2Ctas%3Am"},
+			{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAScAAAAJDdmZmMyYzZjLTA5ZGUtNGRiNi05ZTM0LWE1ZDAyYmY2ZjlmNw.jpg", name:"Analben Manakiwala", url:"https://www.linkedin.com/in/analben-manakiwala?authType=NAME_SEARCH&authToken=k1_c&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A483145149%2CauthType%3ANAME_SEARCH%2Cidx%3A1-2-2%2CtarId%3A1470612984683%2Ctas%3Aa"}
+		],
+		"static/images/wordwars.png",
+		"http://wordwarsapp.herokuapp.com"
+	);
+
+	experiment[2] = new hbproto(
+		"Poem<br>Comber", 
+		"This application was built to allow teachers and students to communally learn from and dissect literature. Students and Professors log into their accounts, where Professors will create assignments for the students to critique by clicking on passages and leaving comments. Students have access to all other student's comments. We used JSON web tokens, MySql for storing the accounts, texts, and comments, Node.js and Bootstrap.", 
+		[
+			{image:"http://sjgraboski.com/wp-content/uploads/2016/06/headshot.jpg", name:"Steve Graboski", url:"https://www.linkedin.com/in/sjgraboski?authType=NAME_SEARCH&authToken=54NY&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A133967732%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1470613252610%2Ctas%3Asteve"},
+			{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAcwAAAAJGI0OGNmOGRlLTQxNzktNGI3Mi04OGZlLTA0NjdlZjhkNGZkMw.jpg",name:"Andrew Vanderhoff", url:"https://www.linkedin.com/in/andrew-vanderhoff-37008820?authType=NAME_SEARCH&authToken=E37E&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A71843844%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1470613278275%2Ctas%3Aa"},
+			{image:"static/images/Ana.png",name:"Analben Manakiwala",url:"https://www.linkedin.com/in/analben-manakiwala?authType=NAME_SEARCH&authToken=k1_c&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A483145149%2CauthType%3ANAME_SEARCH%2Cidx%3A1-2-2%2CtarId%3A1470612984683%2Ctas%3Aa"}
+		], 
+		"static/images/pcomber.png",
+		"https://poemcomber.herokuapp.com"
+	);
+
+	experiment[3] = new hbproto(
+		"Community<br>Code", 
+		"This application is designed to give beginning programmers a library of functions they can reference, curate, and learn from. We developed this as a way to help our classroom learning as we delved into Rutgers Coding Bootcamp. Many of the students in the class contributed to it, and I found it an easy way to learn useful functions from other students.", 
+		[
+			{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAfgAAAAJGVmOGNmOTZlLWVlNjQtNDRiMS1hY2MzLTY0N2FiN2Q3MTM4OQ.jpg", name:"Kevin Graber", url:"https://www.linkedin.com/in/kevin-graber-310?authType=NAME_SEARCH&authToken=bGpb&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A423997440%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1470613273776%2Ctas%3Akevin"},
+			{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAiuAAAAJDVlMjgxZjE4LTNmOTItNDI2ZS1iZDE3LTQxYjM4ZmZkYmJiZA.jpg",name:"Mauricio Ramos", url:"https://www.linkedin.com/in/maramos1126?authType=NAME_SEARCH&authToken=xByp&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A163957937%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1470613259426%2Ctas%3Amau"}
+		], 
+		"static/images/ccode.png",
+		"http://communitycode.herokuapp.com/"
+	);
+
+	experiment[4] = new hbproto(
+
+		"Hangman!",
+		"This was the first webpage I ever made using javascript. Even looking back now I'm kind of pleased with myself. Learning how to mess with the DOM and work with audio was a lot of fun.",
+		null,
+		"static/images/hman.png",
+		"http://hangman-z.herokuapp.com/"
+
+	);
+
+	module.exports = experiment;
--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -4,35 +4,16 @@ var path = require('path');
 
 var app = express();
 
+var experiment = require('./hb-html-page-objects.js')
+
+console.log(experiment);
+
 module.exports = function(app){
 
 	//PAGES
-
-	function hbproto(title, paragraph, person, imgsrc, url){
-		this.title = title;
-		this.paragraph = paragraph;
-		this.person = person;
-		this.imgsrc = imgsrc;
-		this.url = url; 
-	}
-
-	var experiment = {
-		title: "Word Wars!",
-		paragraph: "Is a typing game created by our small team of web developers. It features a library of words from the Wordnik API, high scores stored in MySql, fun graphic and sound design. We used Firebase for user authentication, dynamic room creation, a chat service, and multiplayer interaction. We are very proud of our product created in just a few weeks, and intend to continue working on it by adding features in the future.",
-		person: [
-		{image:"static/images/Lou.png", name:"Louis Delia"},
-		{image:"static/images/Matt.png", name:"Matthew Martin"},
-		{image:"static/images/Ana.png", name:"Analben Manakiwala"}
-		// {image:"http://imgur.com/a/1bZOH/", name:"Cave Paintings"}
-		],
-		imgsrc:"static/images/wordwars.png",
-		url: "http://wordwarsapp.herokuapp.com"
-	}
-
-	var experiment2 = new hbproto("Poem<br>Comber", "This application was built to allow teachers and students to communally learn from and dissect literature. Students and Professors log into their accounts, where Professors will create assignments for the students to critique by clicking on passages and leaving comments. Students have access to all other student's comments. We used JSON web tokens, MySql for storing the accounts, texts, and comments, Node.js and Bootstrap.", [{image:"http://sjgraboski.com/wp-content/uploads/2016/06/headshot.jpg", name:"Steve Graboski"},{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAcwAAAAJGI0OGNmOGRlLTQxNzktNGI3Mi04OGZlLTA0NjdlZjhkNGZkMw.jpg",name:"Andrew Vanderhoff"},{image:"static/images/Ana.png",name:"Analben Manakiwala"}], "static/images/pcomber.png","https://poemcomber.herokuapp.com");
-
-	var experiment3 = new hbproto("Community<br>Code", "This application is designed to give beginning programmers a library of functions they can reference, curate, and learn from. We developed this as a way to help our classroom learning as we delved into Rutgers Coding Bootcamp. Many of the students in the class contributed to it, and I found it an easy way to learn useful functions from other students.", [{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAfgAAAAJGVmOGNmOTZlLWVlNjQtNDRiMS1hY2MzLTY0N2FiN2Q3MTM4OQ.jpg", name:"Kevin Graber"},{image:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAiuAAAAJDVlMjgxZjE4LTNmOTItNDI2ZS1iZDE3LTQxYjM4ZmZkYmJiZA.jpg",name:"Mauricio Ramos"}], "static/images/ccode.png","http://communitycode.herokuapp.com/");
-
+	app.get('/home', function(req, res){
+		res.render('home');
+	});
 	app.get('/portfolio', function(req, res){
 		res.render('portfolio');
 	});
@@ -44,13 +25,16 @@ module.exports = function(app){
 	});
 
 	app.get('/wordwars', function(req, res){
-		res.render('wordwars', experiment);
+		res.render('team', experiment[1]);
 	});
 	app.get('/poemcomber', function(req, res){
-		res.render('wordwars', experiment2);
+		res.render('team', experiment[2]);
 	});
 	app.get('/communitycode', function(req, res){
-		res.render('wordwars', experiment3);
+		res.render('team', experiment[3]);
+	});
+	app.get('/hangman', function(req, res){
+		res.render('noteam', experiment[4]);
 	});
 
 
